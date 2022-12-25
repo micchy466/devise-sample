b5a10ccc432b87388397a80a3b5f60c99356d313
micchy466 <48757027+micchy466@users.noreply.github.com>
2022-12-25 16:31:10 +0900

    👔 Deviseログイン時usernameとemail両方でログインできる仕様に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+
+  validates :username, presence: true, uniqueness: true
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
